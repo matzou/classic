20acda1718143ae0c54efd45ab85238148aa51e2
matzou <79103646+matzou@users.noreply.github.com>
2021-11-15 15:48:38 +0900

    Add files via upload

---
+++ symphony.js
@@ -0,0 +1,35 @@
+$(function(){
+    $("#button").bind("click",function(){
+
+    var abc , def;
+    abc = $("#series").val();
+    def = $("#level").val();
+    ghi = $("#search").val();
+    re = new RegExp(abc);
+    re2 = new RegExp(def);
+    re3 = new RegExp(ghi);
+
+        $("#data tbody tr").each(function(){
+            var txt = $(this).find("td").text();
+            if(txt.match(re) != null){
+
+              if(txt.match(re2) != null){
+
+                if(txt.match(re3) != null){
+                    $(this).show();
+                }else{
+                    $(this).hide();
+                }
+              }else{
+                  $(this).hide();
+              }
+            }else{
+                $(this).hide();
+            }
+        });
+    });
+
+    $("#button2").bind("click",function(){
+        $("#data tr").show();
+    });
+});
